08cdd890e09bfa7ac5a8d17078008c9e68d2bb60
SEVVALKARABAL <sevvallkaraball@hotmail.com>
2025-01-17 20:09:13 +0300

    second version

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ export default function App() {
         </form>
 
         <p className='mt-10 text-center text-sm text-gray-500'>
-          Üye değil misiniz?{' '}
+          Üye misiniz ?{' '}
           <a
             href='#'
             className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
